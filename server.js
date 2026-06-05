3cacfd75ebb01733efe50ba581b6bfb72b2ec06e
Niha255 <manoniharikaterli255@gmail.com>
2026-06-05 21:59:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// 
 const productRoutes = require("./productRoutes");
 app.use("/api/products", productRoutes);
 
-// MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/krishiDB")
+// 
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-// Start server
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+// 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
